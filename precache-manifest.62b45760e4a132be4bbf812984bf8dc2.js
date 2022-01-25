e2d3c6447b13f8892d214ef8c60aa5def44097fe
unknown <hcristalv@gmail.com>
2022-01-25 16:57:01 -0600

    Updates

---
+++ precache-manifest.62b45760e4a132be4bbf812984bf8dc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3233d9891a93f9084dc1e4d28b09daa",
+    "url": "/app-covid-2022/index.html"
+  },
+  {
+    "revision": "27f542efccc2516394b3",
+    "url": "/app-covid-2022/static/css/main.00704809.chunk.css"
+  },
+  {
+    "revision": "7f22ee2ae502552c8664",
+    "url": "/app-covid-2022/static/js/2.c84d5016.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/app-covid-2022/static/js/2.c84d5016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f542efccc2516394b3",
+    "url": "/app-covid-2022/static/js/main.a7aab1b1.chunk.js"
+  },
+  {
+    "revision": "94f984945b02d244e53b",
+    "url": "/app-covid-2022/static/js/runtime-main.e09a9fd2.js"
+  }
+]);
